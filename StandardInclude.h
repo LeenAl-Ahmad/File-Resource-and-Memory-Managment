43748ec946a89385f7a7872323a1b03ad251c3e5
Leen Al-Ahmad <alahmadl@sheridancollege.ca>
2025-01-24 13:57:58 -0500

    File was read successfully

---
+++ StandardInclude.h
@@ -0,0 +1,30 @@
+#pragma once
+#ifndef STANDARD_INCLUDES_H
+#define STANDARD_INCLUDES_H
+
+// Include standard headers
+#include <string>
+#include <vector>
+#include <iostream>
+#include <cinttypes>
+#include <stdio.h>
+#include <thread>
+
+// Project headers
+#include "Singleton.h"
+
+// OS specific includes and defines
+#ifdef _WIN32
+#include <windows.h>
+#include <direct.h>
+#define M_ASSERT(_cond, _msg) \
+    if (!(_cond)) { OutputDebugStringA(_msg); std::abort(); }
+#define GetCurrentDir _getcwd
+#else
+#include <unistd.h>
+#define GetCurrentDir getcwd
+#endif
+
+using namespace std;
+
+#endif // STANDARD_INCLUDES_H
